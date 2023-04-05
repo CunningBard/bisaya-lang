113bb82f071e59ac39c110333856dccdcb48af3d
Eric <jhonjhonjumaday@gmail.com>
2023-04-05 21:20:47 +0800

    version 0.1 completed, Good Bye

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use std::collections::VecDeque;
 use std::fs;
+use std::process::exit;
 use pest::Parser;
 use crate::parser::{Rule, BareParser, parse_file_data, Statement};
 use crate::eval_parser::{ExprAst, parse_expressions};
@@ -17,12 +19,21 @@ use crate::virtual_machine::{Instruction, IntValue, ObjectCreator, Value, ValueT
 
 fn main() {
     let mut debug_mode = false;
-    for arg in std::env::args() {
-        if arg == "--debug" {
-            debug_mode = true;
+    let mut args: VecDeque<String> = std::env::args().into_iter().collect();
+    let _executable_path = args.pop_front().expect("impossible");
+    let file_name = args.pop_front().expect("no file name given");
+    if file_name == "--help" || file_name == "-h" {
+        println!("Usage: {} <file> [--debug]", _executable_path);
+        return;
+    }
+    for arg in args {
+        match &*arg {
+            "--debug" | "-d" => debug_mode = true,
+            "--help"  | "-h"=> { println!("Usage: {} <file> [--debug]", _executable_path); exit(0) },
+            _ => unimplemented!("{} has not been implemented or its invalid", arg)
         }
     }
-    let file_contents = fs::read_to_string("test.txt").expect("couldnt read file");
+    let file_contents = fs::read_to_string(file_name).expect("couldnt read file");
     let statements = parse_file_data(&file_contents);
     let (instructions, function_locations, class_creators) =  instruction_compiler::compile(statements.clone());
 
